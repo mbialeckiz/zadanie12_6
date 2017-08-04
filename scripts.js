f241fd3406c030563058a8e533188cb94c33db7a
mbialeckiz <marcinbialeckiz@gmail.com>
2017-08-04 15:14:25 +0200

    second commit

--- scripts.js
+++ scripts.js
@@ -1,40 +1,21 @@
-var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-var prefix = "https://cors-anywhere.herokuapp.com/";
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
 
-function getQuote() {
-    $.getJSON(prefix + quoteUrl, createTweet);
-    $.ajaxSetup({ 
-      cache: false 
+$('button').click(searchCountries); // button dostaje żądanie searchCountries
+
+function searchCountries() {
+    var countryName = $('#country-name').val(); // nazwa wpisana przez usera zostaje przyporządkowana do countryName
+if(!countryName.length) countryName = 'Papua New Guinea'; // Jeśli pole puste - automatycznie wpisana wartość docelowa
+$.ajax({
+      url: url + countryName, // url strony plus nazwa wpisana przez usera
+      method: 'GET', // metoda pobierz
+      success: showCountriesList // zwrócenie wyniku
     });
 }
 
-function createTweet(input) {
-    var data = input[0];
-
-    var quoteText = $(data.content).text().trim();
-    var quoteAuthor = data.title;
-
-    if (!quoteAuthor.length) {
-        quoteAuthor = "Unknown author";
-    }
-  
-  var tweetText = "Quote of the day : " + quoteText + " Author: " + quoteAuthor;
-  
-  if (tweetText.length > 140) {
-    getQuote();
-  } else {
-    var tweet = tweetLink + encodeURIComponent(tweetText);
-    $('.quote').text(quoteText);
-    $('.author').text("Author: " + quoteAuthor);
-    $('.tweet').attr('href', tweet);
-  }
-  
-}
-
-$(document).ready(function() {
-      getQuote();
-      $('.trigger').click(function() {
-          getQuote();
-      })
+function showCountriesList(resp) { // funkcja odp. za pokazanie listy krajów
+  countriesList.empty(); // wyczyszczenie listy przed każdym wyszukiwaniem
+resp.forEach(function(item) {
+  $('<li>').text(item.name).appendTo(countriesList); // dodanie nowego elementu
 });
+}
